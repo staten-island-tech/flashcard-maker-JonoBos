7d55ee2a392e418ff3aabf80711bf5b57472d5b1
Jonoth bosque-garcia <jonoth.bosque-garcia@C56.local>
2025-03-20 10:15:49 -0400

    yep

--- dawork.py
+++ dawork.py
@@ -1,13 +1,49 @@
 import json 
-x = open("./flashcard.json", encoding="utf8")
-data = json.load(x)
 
 class Teacher:
-    key_teacher = input("What word or phrase would you like to use?")
-    answer_teacher = input("what is the value/answer?")
+    def card_creator():
+        cards = {}
+        finished = False 
+        while finished == False:
+            if input ("type YES if you would like to make a card") == 'YES':
+                cards(input('Type the question:')) == input('Type in your answer')
+            else: 
+                finished == True
+                with open("flashcard.json", "w") as file:
+                    json.dump(cards,file, indent =4)
 
-flashcard_list =[]
-flashcard_data = [flashcard.to_dict() for flashcard in flashcards_list]
-with open("flashcards.json", "w") as file:
-    json.dump(flashcard_data, file, indent=4)
 
+class Student:
+#From Json file you want the student to find the value 
+    def card_answer():
+        value_student=input("What is the answer?")
+        point ==0
+        streak==0
+        with open ("flashcard.json", "r") as file:
+            student_card =json.load(file)
+            for card in student_card:
+                print (f'card')
+                if value_student == student_card [card]: 
+                    point +=1 
+                    streak+=1
+                if value_student != student_card [card]:
+                    streak = 0
+                    print (f' Sorry the correct answer was ' ,{card.value()} ,'Your streak has been reset.')
+                print (f'You have finished. Your score was {point}')
+
+while True:
+
+    role_decided = False 
+
+    while role_decided == False:
+        teacher_student = input ("Type T if you're a teacher or type S if you're a student.")
+        if teacher_student == 'T':
+            Teacher.card_creator()
+            role_decided == True
+        elif teacher_student == 'S':
+            Student.card_answer()
+            role_decided ==True
+        else:
+            print ("No no no")
+if streak>=5:
+    point+=10
