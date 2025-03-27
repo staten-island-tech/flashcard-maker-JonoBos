b203ffc3f3d4c814157e19238947d1c40af73e85
JonoBos <jonothanb@nycstudents.net>
2025-03-27 17:43:43 -0400

    f

--- dawork.py
+++ dawork.py
@@ -10,30 +10,32 @@ class Teacher:
                 answer = input("Gimme an answer")
                 cards[question] = answer
             else: 
-                finished == True
-                with open("flashcard.json", "w") as file:
-                    json.dump(cards,file, indent =4)
+                break
+                
+        finished = True
+        with open("FlashCards.json", "w") as file:
+            json.dump(cards,file, indent =4)
 
 
 class Student:
     def card_answer():
         value_student=input("What is the answer?")
-        point ==0
-        streak==0
-        with open ("flashcard.json", "r") as file:
+        point =0
+        streak=0
+        with open ("FlashCards.json", "r") as file:
             student_card =json.load(file)
             for card in student_card:
                 print (f'card')
                 if value_student == student_card [card]: 
                     point +=1 
                     streak+=1
+                    if streak>=5:
+                        point+=10
                 if value_student != student_card [card]:
                     streak = 0
-                    print (f' Sorry the correct answer was ' ,{card.value()} ,'Your streak has been reset.')
+                    print (f' Sorry the correct answer was ' ,{student_card[card]} ,'Your streak has been reset.')
                 print (f'You have finished. Your score was {point}')
-
-while True:
-
+def main ():
     role_decided = False 
 
     while role_decided == False:
@@ -46,8 +48,6 @@ while True:
             role_decided ==True
         else:
             print ("No no no")
-if streak>=5:
-    point+=10
 
-if __name__ == "__main__":
-    main()
+
+
